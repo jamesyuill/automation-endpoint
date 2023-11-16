7c65bde1df9eb0fcc44605be6b815bc7bf9d5ee4
James Yuill <jamesyuill@jamess-mbp.lan>
2023-11-16 19:32:32 +0000

    first commit - sets up endpoint to mimic northcoders applicant api

---
+++ app.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import { readFile } from 'node:fs/promises';
+import { writeFile } from 'node:fs';
+import { endpoints } from './endpoints.js';
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({ endpoints });
+});
+
+app.get('/applicants', async (req, res) => {
+  try {
+    const filePath = new URL('data/applicantData.json', import.meta.url);
+    const applicants = await readFile(filePath, { encoding: 'utf8' });
+    res.send(applicants);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.get('/applicants/:id', async (req, res) => {
+  const { id } = req.query;
+  try {
+    const filePath = new URL('data/applicantData.json', import.meta.url);
+    const applicants = await readFile(filePath, { encoding: 'utf8' });
+    const applicant = await JSON.parse(applicants).filter(
+      (app) => app.id == id
+    );
+    res.send(applicant);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.post('/applicants/:id', async (req, res) => {
+  const id = req.query.id;
+  const { entryBooked } = req.body;
+  try {
+    const filePath = new URL('data/applicantData.json', import.meta.url);
+    const applicants = await readFile(filePath, { encoding: 'utf8' });
+    const updatedApplicants = await JSON.parse(applicants).map((app) => {
+      if (app.id === id) {
+        let newObj = {
+          ...app,
+          entryBooked,
+        };
+        return newObj;
+      }
+      return app;
+    });
+
+    writeFile(
+      'data/applicantData.json',
+      JSON.stringify(updatedApplicants),
+      (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }
+    );
+
+    const applicant = updatedApplicants.filter((app) => app.id == id);
+    res.send(applicant);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
---
+++ endpoints.js
@@ -0,0 +1,5 @@
+export const endpoints = [
+  '/applicants - Get a list of all applicants',
+  '/applicants/:id - Get specific applicant by ID',
+  '/applicants/:id - Post or update the entryBooked status',
+];
